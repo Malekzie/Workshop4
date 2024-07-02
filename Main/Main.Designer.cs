6299512967017208249171950ae78c9885e901ad
Robbie Soriano <grapeberryfruit@gmail.com>
2024-07-02 10:48:07 -0600

    Previous commit message

---
+++ Main/Main.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Main
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            viewPkg = new Button();
+            viewProd = new Button();
+            viewSup = new Button();
+            viewProdSup = new Button();
+            panel1 = new Panel();
+            btnRemove = new Button();
+            btnModify = new Button();
+            btnAdd = new Button();
+            btnSubmit = new Button();
+            txtQuery = new TextBox();
+            dgvView = new DataGridView();
+            flowLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvView).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.BackColor = Color.Cyan;
+            flowLayoutPanel1.Controls.Add(viewPkg);
+            flowLayoutPanel1.Controls.Add(viewProd);
+            flowLayoutPanel1.Controls.Add(viewSup);
+            flowLayoutPanel1.Controls.Add(viewProdSup);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(237, 904);
+            flowLayoutPanel1.TabIndex = 0;
+            flowLayoutPanel1.WrapContents = false;
+            // 
+            // viewPkg
+            // 
+            viewPkg.AutoSize = true;
+            viewPkg.BackColor = Color.FromArgb(10, 155, 170);
+            viewPkg.Dock = DockStyle.Fill;
+            viewPkg.FlatAppearance.BorderColor = Color.FromArgb(41, 53, 65);
+            viewPkg.FlatAppearance.BorderSize = 0;
+            viewPkg.FlatAppearance.MouseDownBackColor = Color.FromArgb(240, 237, 183);
+            viewPkg.FlatStyle = FlatStyle.Flat;
+            viewPkg.ForeColor = SystemColors.Window;
+            viewPkg.ImageAlign = ContentAlignment.MiddleLeft;
+            viewPkg.Location = new Point(3, 3);
+            viewPkg.Name = "viewPkg";
+            viewPkg.Size = new Size(231, 37);
+            viewPkg.TabIndex = 0;
+            viewPkg.Text = "Packages";
+            viewPkg.UseVisualStyleBackColor = false;
+            viewPkg.Click += viewPkg_Click;
+            // 
+            // viewProd
+            // 
+            viewProd.AutoSize = true;
+            viewProd.BackColor = Color.FromArgb(10, 155, 170);
+            viewProd.Dock = DockStyle.Fill;
+            viewProd.FlatAppearance.BorderColor = Color.FromArgb(41, 53, 65);
+            viewProd.FlatAppearance.BorderSize = 0;
+            viewProd.FlatAppearance.MouseDownBackColor = Color.FromArgb(240, 237, 183);
+            viewProd.FlatStyle = FlatStyle.Flat;
+            viewProd.ForeColor = SystemColors.Window;
+            viewProd.ImageAlign = ContentAlignment.MiddleLeft;
+            viewProd.Location = new Point(3, 46);
+            viewProd.Name = "viewProd";
+            viewProd.Size = new Size(231, 37);
+            viewProd.TabIndex = 1;
+            viewProd.Text = "Products";
+            viewProd.UseVisualStyleBackColor = false;
+            viewProd.Click += viewProd_Click;
+            // 
+            // viewSup
+            // 
+            viewSup.AutoSize = true;
+            viewSup.BackColor = Color.FromArgb(10, 155, 170);
+            viewSup.Dock = DockStyle.Fill;
+            viewSup.FlatAppearance.BorderColor = Color.FromArgb(41, 53, 65);
+            viewSup.FlatAppearance.BorderSize = 0;
+            viewSup.FlatAppearance.MouseDownBackColor = Color.FromArgb(240, 237, 183);
+            viewSup.FlatStyle = FlatStyle.Flat;
+            viewSup.ForeColor = SystemColors.Window;
+            viewSup.ImageAlign = ContentAlignment.MiddleLeft;
+            viewSup.Location = new Point(3, 89);
+            viewSup.Name = "viewSup";
+            viewSup.Size = new Size(231, 37);
+            viewSup.TabIndex = 2;
+            viewSup.Text = "Suppliers";
+            viewSup.UseVisualStyleBackColor = false;
+            viewSup.Click += viewSup_Click;
+            // 
+            // viewProdSup
+            // 
+            viewProdSup.AutoSize = true;
+            viewProdSup.BackColor = Color.FromArgb(10, 155, 170);
+            viewProdSup.Dock = DockStyle.Fill;
+            viewProdSup.FlatAppearance.BorderColor = Color.FromArgb(41, 53, 65);
+            viewProdSup.FlatAppearance.BorderSize = 0;
+            viewProdSup.FlatAppearance.MouseDownBackColor = Color.FromArgb(240, 237, 183);
+            viewProdSup.FlatStyle = FlatStyle.Flat;
+            viewProdSup.ForeColor = SystemColors.Window;
+            viewProdSup.ImageAlign = ContentAlignment.MiddleLeft;
+            viewProdSup.Location = new Point(3, 132);
+            viewProdSup.Name = "viewProdSup";
+            viewProdSup.Size = new Size(231, 37);
+            viewProdSup.TabIndex = 3;
+            viewProdSup.Text = "Product Suppliers";
+            viewProdSup.UseVisualStyleBackColor = false;
+            viewProdSup.Click += viewProdSup_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Info;
+            panel1.Controls.Add(btnRemove);
+            panel1.Controls.Add(btnModify);
+            panel1.Controls.Add(btnAdd);
+            panel1.Controls.Add(btnSubmit);
+            panel1.Controls.Add(txtQuery);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(237, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1527, 150);
+            panel1.TabIndex = 1;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(313, 59);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(112, 34);
+            btnRemove.TabIndex = 4;
+            btnRemove.Text = "Remove";
+            btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            btnModify.Location = new Point(172, 58);
+            btnModify.Name = "btnModify";
+            btnModify.Size = new Size(112, 34);
+            btnModify.TabIndex = 3;
+            btnModify.Text = "Modify";
+            btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(32, 59);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(112, 34);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(1403, 61);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(112, 34);
+            btnSubmit.TabIndex = 1;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // txtQuery
+            // 
+            txtQuery.Location = new Point(1130, 61);
+            txtQuery.Name = "txtQuery";
+            txtQuery.PlaceholderText = "Search...";
+            txtQuery.Size = new Size(248, 31);
+            txtQuery.TabIndex = 0;
+            // 
+            // dgvView
+            // 
+            dgvView.AllowUserToAddRows = false;
+            dgvView.AllowUserToDeleteRows = false;
+            dgvView.AllowUserToResizeColumns = false;
+            dgvView.AllowUserToResizeRows = false;
+            dgvView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvView.Dock = DockStyle.Fill;
+            dgvView.Location = new Point(237, 150);
+            dgvView.Name = "dgvView";
+            dgvView.RowHeadersWidth = 62;
+            dgvView.Size = new Size(1527, 754);
+            dgvView.TabIndex = 2;
+            dgvView.CellClick += dgvView_CellClick;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1764, 904);
+            Controls.Add(dgvView);
+            Controls.Add(panel1);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Main";
+            Text = "Dashboard";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private Button viewPkg;
+        private Button viewProd;
+        private Button viewSup;
+        private Button btnSubmit;
+        private TextBox txtQuery;
+        private DataGridView dgvView;
+        private Button btnRemove;
+        private Button btnModify;
+        private Button btnAdd;
+        private Button viewProdSup;
+    }
+}
---
+++ Main/Main.cs
@@ -0,0 +1,51 @@
+using Main.Utils; // Make sure to include this namespace
+using TravelExpertsData.DataAccess;
+
+namespace Main
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData<T>(List<T> data, string dataType)
+        {
+            // Clears the data grid view
+            dgvView.DataSource = null;
+            // Replaces the data grid view data source with the new data
+            dgvView.DataSource = data;
+            ColRename.RenameColumns(dgvView, dataType);
+        }
+
+        private void dgvView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Handle cell click events if necessary
+        }
+
+        private void viewPkg_Click(object sender, EventArgs e)
+        {
+            var data = DataCache.Instance.Packages;
+            LoadData(data, "PackageDTO");
+        }
+
+        private void viewProd_Click(object sender, EventArgs e)
+        {
+            var data = DataCache.Instance.Products;
+            LoadData(data, "ProductDTO");
+        }
+
+        private void viewSup_Click(object sender, EventArgs e)
+        {
+            var data = DataCache.Instance.Suppliers;
+            LoadData(data, "SupplierDTO");
+        }
+
+        private void viewProdSup_Click(object sender, EventArgs e)
+        {
+            var data = DataCache.Instance.ProductSuppliers;
+            LoadData(data, "ProductSupplierDTO"); // Adjust as needed
+        }
+    }
+}
