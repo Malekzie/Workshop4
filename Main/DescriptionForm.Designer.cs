a4d062c27ec6288e48b7ac58104d4c3636e56a53
Robbie <133696122+Malekzie@users.noreply.github.com>
2024-07-10 02:00:16 -0600

    Integrated Rich Text Editor

---
+++ Main/DescriptionForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Main
+{
+    partial class DescriptionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Dock = DockStyle.Fill;
+            webView21.Location = new Point(0, 0);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(1245, 581);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            // 
+            // DescriptionForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(31, 34, 53);
+            ClientSize = new Size(1245, 581);
+            Controls.Add(webView21);
+            ForeColor = SystemColors.ControlLightLight;
+            Name = "DescriptionForm";
+            Text = "Description";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
---
+++ Main/DescriptionForm.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class DescriptionForm : Form
+    {
+        private string initialContent;
+
+        public DescriptionForm(string content)
+        {
+            initialContent = content;
+            InitializeComponent();
+            InitializeAsync();
+
+        }
+
+        private async void InitializeAsync()
+        {
+            var env = await CoreWebView2Environment.CreateAsync(null, null, new CoreWebView2EnvironmentOptions("--remote-debugging-port=9222"));
+            await webView21.EnsureCoreWebView2Async(env);
+            LoadTinyMCE();
+        }
+
+        private void LoadTinyMCE()
+        {
+            string htmlFilePath = Path.Combine(Application.StartupPath, "editor.html");
+
+            if (File.Exists(htmlFilePath))
+            {
+                // Modify HTML content to include the initial content
+                string htmlContent = File.ReadAllText(htmlFilePath);
+                htmlContent = htmlContent.Replace("<textarea id=\"mytextarea\"></textarea>", $"<textarea id=\"mytextarea\">{initialContent}</textarea>");
+                webView21.NavigateToString(htmlContent);
+            }
+            else
+            {
+                MessageBox.Show($"HTML file for TinyMCE editor not found at path: {htmlFilePath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public string GetContent()
+        {
+            // Implement a way to retrieve content from TinyMCE (e.g., through JavaScript)
+            // This is a placeholder; actual implementation may vary
+            return initialContent;
+        }
+
+
+    }
+}
