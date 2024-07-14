73ba1a9c7e810f0d8a4b318e5bebfb185fc1e266
Ryan Medeiros <margaret.atwood.1984@outlook.com>
2024-07-14 04:29:16 -0600

    Added comments to the primary form's code.

--- Main/Main.cs
+++ Main/Main.cs
@@ -1,63 +1,82 @@
-using Main.Utils; // Make sure to include this namespace
+// Threaded Project 2 Workshop 4 
+// Created by Robbie Soriano
+// Modified by Ryan Medeiros
+// Defines our primary form as called my main() in Program.cs
+
+//Application dependencies
+using Main.Utils;
 using TravelExpertsData.DataAccess;
 
 namespace Main
 {
     public partial class Main : Form
     {
+        //Initializes our primary form. 
         public Main()
         {
             InitializeComponent();
         }
 
+        //Refreshes the DataGridView
         private void LoadData<T>(List<T> data, string dataType)
         {
-            // Clears the data grid view
+            // Clear the data grid view
             dgvView.DataSource = null;
-            // Replaces the data grid view data source with the new data
+
+            // Replace the data grid view data source with our provided list
             dgvView.DataSource = data;
+
+            //Rename our DataGridView column using the given formatted string
             ColRename.RenameColumns(dgvView, dataType);
         }
 
+        // Handles cell click events if necessary
         private void dgvView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            // Handle cell click events if necessary
+
         }
 
+        //If the user clicks the "Packages" button, 
         private void viewPkg_Click(object sender, EventArgs e)
         {
-            var data = DataCache.Instance.Packages;
-            LoadData(data, "PackageDTO");
+            //Copy the cached database entries to the DataGridView
+            LoadData(DataCache.Instance.Packages, "PackageDTO");
         }
 
+        //If the user clicks the "Products" button, 
         private void viewProd_Click(object sender, EventArgs e)
         {
-            var data = DataCache.Instance.Products;
-            LoadData(data, "ProductDTO");
+            //Copy the cached database entries to the DataGridView
+            LoadData(DataCache.Instance.Products, "ProductDTO");
         }
 
+        //If the user clicks the "Suppliers" button, 
         private void viewSup_Click(object sender, EventArgs e)
         {
-            var data = DataCache.Instance.Suppliers;
-            LoadData(data, "SupplierDTO");
+            //Copy the cached database entries to the DataGridView
+            LoadData(DataCache.Instance.Suppliers, "SupplierDTO");
         }
 
+        // If the user clicks the "Product Suppliers" button,
         private void viewProdSup_Click(object sender, EventArgs e)
         {
-            var data = DataCache.Instance.ProductSuppliers;
-            LoadData(data, "ProductSupplierDTO"); // Adjust as needed
+            //Copy the cached database entries to the DataGridView
+            LoadData(DataCache.Instance.ProductSuppliers, "ProductSupplierDTO");
         }
 
+        // If the user clicks the "Exit" button,
         private void btnExit_Click(object sender, EventArgs e)
         {
+            // Close the application.
             Application.Exit();
         }
 
-
+        // If the user clicks either "Add" or "Modify", 
         private void OpenAddModifyForm(string dataType)
         {
             using (var form = new AddModifyPackages())
             {
+                // Show the necessary modal form. 
                 form.ShowDialog();
             }
         }
